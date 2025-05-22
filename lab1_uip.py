2ccab3779971fbc83747e6a686d179a2dd077ac7
dimyk <dmytro.mykhalchuk.pp.2022@lpnu.ua>
2025-05-22 22:04:54 +0300

    Full code

--- lab1_uip.py
+++ lab1_uip.py
@@ -1,5 +1,3 @@
-# car_service_tracker.py
-
 import json
 import os
 from datetime import datetime
@@ -38,16 +36,31 @@ def show_history(data):
     else:
         print("Історії не знайдено.")
 
+#Додаткова функція
+def show_last_service(data):
+    if not data:
+        print("Немає записів.")
+        return
+    for vin, records in data.items():
+        if records:
+            last = sorted(records, key=lambda r: r['date'], reverse=True)[0]
+            print(f"{vin}: {last['date']} | {last['service']} @ {last['mileage']} км")
+        else:
+            print(f"{vin}: немає обслуговувань")
+
+
 def main():
     data = load_data()
     while True:
-        print("\n1. Додати запис обслуговування\n2. Переглянути історію\n3. Вийти")
+        print("\n1. Додати запис обслуговування\n2. Переглянути історію\n3. Вивести останнє обслуговування\n4. Вийти")
         choice = input("Оберіть дію: ")
         if choice == '1':
             add_service(data)
         elif choice == '2':
             show_history(data)
         elif choice == '3':
+            show_last_service(data)
+        elif choice == '4':
             break
         else:
             print("Невірний вибір.")
